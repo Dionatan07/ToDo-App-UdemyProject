46b509b07f314d6acb144d8efd2f5cf4b07fb8a7
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-05-26 22:37:50 -0300

    Created Task Controller

---
+++ src/main/java/TodoApp/Main.java
@@ -0,0 +1,18 @@
+package TodoApp;
+
+import java.sql.Connection;
+import util.ConnectionFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Connection c = ConnectionFactory.getConnection();
+
+        ConnectionFactory.closeConnection(c);
+
+        
+        System.out.println(c);
+    }
+
+}
